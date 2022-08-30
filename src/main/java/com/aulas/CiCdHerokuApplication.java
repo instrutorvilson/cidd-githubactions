2535f543b599b84f71f1bb883e0b4d5ba2bb6797
Vilson <instrutorvilsonmoro@gmail.com>
2022-08-30 15:19:03 -0300

    primeiro commit

---
+++ src/main/java/com/aulas/CiCdHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdHerokuApplication.class, args);
+	}
+
+}
